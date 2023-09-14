b1d6b6b2db622eba787682248e71a66a05a19ffb
LiYang <cctv1005s@gmail.com>
2023-09-14 15:56:54 +0800

    fix: style error of body

--- src/components/popup/components/LanguageToggle.tsx
+++ src/components/popup/components/LanguageToggle.tsx
@@ -44,7 +44,7 @@ export const LanguageToggle = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" className="ml-2 rounded-full">
-          <Languages className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Languages className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
